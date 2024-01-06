a9af3d8950891927444a9810cdc3828d62c769c5
Joao <joao.wozniak@gmail.com>
2024-01-06 16:38:04 +0000

    basics

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,13 +1,37 @@
 const Hello = (props) => {
 
   console.log(props)
+  const name = props.name
+  const age = props.age
+  const city = props.city
+
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
   return (
     <div>
       <p>
-
         Hello {props.name}, you are {props.age} years old
+        <p>Born in {bornYear()}</p>
+        <p>Na cityzassa de {city}</p>
       </p>
     </div>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
   )
 }
 
@@ -20,10 +44,67 @@ const App = () => {
     <div>
       <h1>Greetings</h1>
 
-      <Hello name='Maya' age={26 + 10} />
+      <Hello name='Maya' age={26 + 10} city='cityzassa' />
       <Hello name={name} age={age} />
     </div>
   )
 }
 
-export default App
+const Apps = (props) => {
+  const { counter } = props
+  return (
+    <div>{counter}</div>
+  )
+}
+
+
+import { useState } from 'react'
+
+const Appois = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+  // const handleClick = () => {
+  //   console.log('clicked')
+  // }
+
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   1000
+  // )
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
+  )
+}
+
+export default Appois
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+import Appois from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<Appois />)
